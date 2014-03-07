b145b1d52c143e8e5afc51ba8da07f7ba14562ca
yamigo <yamigo1021@gmail.com>
2014-03-07 18:29:25 +0900

    [nVSn_yamigo] - add function(login, signup)

--- nVSn/yamigo/TestClient/src/com/aozi/testclient/LoginActivity.java
+++ nVSn/yamigo/TestClient/src/com/aozi/testclient/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.aozi.testclient;
 
 import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.aozi.testclient.model.TestClientRestApi;
+import com.croquis.util.RestClient.OnRequestComplete;
+import com.croquis.util.RestClient.RestError;
 
 @EActivity(R.layout.login)
 public class LoginActivity extends Activity {
@@ -15,6 +23,9 @@ public class LoginActivity extends Activity {
 	@ViewById
 	EditText password;
 	
+	@Bean
+	TestClientRestApi mTestClientRestApi;
+	
 	@AfterInject
 	void init() {
 		setTitle("LogIn");
@@ -22,6 +33,28 @@ public class LoginActivity extends Activity {
 	
 	@Click
 	void login(){
+		if(TextUtils.isEmpty(email.getText().toString())) {
+			Toast.makeText(this, "email is empty", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		if(TextUtils.isEmpty(password.getText().toString())) {
+			Toast.makeText(this, "password is empty", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		final ProgressDialog progress = ProgressDialog.show(LoginActivity.this, "Login", "wait for a while");
+		mTestClientRestApi.login(email.getText().toString(), password.getText().toString(), new OnRequestComplete<Void>() {
+			@Override
+			public void onComplete(RestError error, Void result) {
+				progress.dismiss();
+				if(error != null) {
+					Toast.makeText(LoginActivity.this, error.error, Toast.LENGTH_LONG).show();
+					return;
+				}
+				
+			}
+		});
 		
 	}
 	
